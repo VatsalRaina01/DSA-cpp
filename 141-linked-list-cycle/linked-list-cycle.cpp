d6475bacf253097104431ad0910f11555268cd62
VatsalRaina01 <154910553+VatsalRaina01@users.noreply.github.com>
2025-07-15 16:58:47 +0530

    Time: 8 ms (81.01%) | Memory: 11.8 MB (53.31%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,14 +12,13 @@ public:
         if(head==NULL||head->next==NULL) return false;
         ListNode* slow=head;
         ListNode* fast=head->next;
-        while(fast!=slow){
-            if(fast->next==NULL||fast->next->next==NULL) return false;
+        while(fast!=NULL&&fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-           
+            if(slow==fast) return true;
 
         }
-        return true;
+        return false;
         
     }
 };
