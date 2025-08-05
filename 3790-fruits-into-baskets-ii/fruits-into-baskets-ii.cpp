c04a97e1474370df9e8d591ce6fab4e8aa785acc
VatsalRaina01 <154910553+VatsalRaina01@users.noreply.github.com>
2025-08-06 02:08:40 +0530

    Time: 10 ms (7.31%) | Memory: 49.1 MB (27.97%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,53 +1,22 @@
 class Solution {
 public:
-    void build(int i, int l, int r, vector<int>& baskets, vector<int>& segmentTree) {
-        if (l == r) {
-            segmentTree[i] = baskets[l];
-            return;
-        }
-        int m = (l + r) / 2;
-
-        build(2*i + 1, l, m, baskets, segmentTree);
-        build(2*i + 2, m + 1, r, baskets, segmentTree);
-
-        segmentTree[i] = max(segmentTree[2*i + 1], segmentTree[2*i + 2]);
-    }
-
-    bool querySegmentTree(int i, int l, int r, vector<int>& segmentTree, int val) {
-        if (segmentTree[i] < val)
-            return false; // No basket in this segment
-
-        if (l == r) {
-            segmentTree[i] = -1; // Mark basket as used
-            return true;
-        }
-
-        int mid = (l + r) / 2;
-        bool placed = false;
-
-        if (segmentTree[2*i + 1] >= val) {
-            placed = querySegmentTree(2*i + 1, l, mid, segmentTree, val);
-        } else {
-            placed = querySegmentTree(2*i + 2, mid + 1, r, segmentTree, val);
-        }
-
-        segmentTree[i] = max(segmentTree[2*i + 1], segmentTree[2*i + 2]);
-
-        return placed;
-    }
-
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
-
-        vector<int> segmentTree(4 * n, -1);
-
-        build(0, 0, n - 1, baskets, segmentTree);
-
+        vector<bool> used(n, false);  // Track used baskets
         int unplaced = 0;
-        for (int &fruit : fruits) {
-            if (!querySegmentTree(0, 0, n - 1, segmentTree, fruit)) {
-                unplaced++;
+
+        for (int i = 0; i < n; i++) {
+            bool placed = false;
+
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
             }
+
+            if (!placed) unplaced++;
         }
 
         return unplaced;
